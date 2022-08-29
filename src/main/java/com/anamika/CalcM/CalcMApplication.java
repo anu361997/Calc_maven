e6e8736b94e8bbbcb01a9e2cb53de720b23c13cf
anu361997 <anamikabasral97@gmail.com>
2022-08-29 12:14:46 +0530

    first commit

---
+++ src/main/java/com/anamika/CalcM/CalcMApplication.java
@@ -0,0 +1,13 @@
+package com.anamika.CalcM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcMApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/anamika/CalcM/CalcMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anamika.CalcM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcMApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
